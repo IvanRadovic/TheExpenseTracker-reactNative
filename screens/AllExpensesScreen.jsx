72dd9edfb0c828963dcd2560ed01788c3b5282d9
Ivan <ivan.radovic@acme.co.me>
2023-04-12 07:11:22 +0200

    addingNavigation

---
+++ screens/AllExpensesScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const AllExpensesScreen = () => {
+    return (  
+        <View>
+            <Text>Ovo je main Page !!</Text>
+        </View>
+    );
+}
+ 
+export default AllExpensesScreen;
